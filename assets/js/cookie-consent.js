e7bdb1f7218f5845ce44b818cfd4cd372b7126f2
Marco Gramaglia <m.gramaglia@gmail.com>
2023-05-31 14:29:28 +0000

    cookie consent

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,15 @@
+window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#314D8A"
+      },
+      "button": {
+        "background": "#DBE5F1"
+      }
+    },
+    "type": "opt-in",
+    "content": {
+      "message": "This website uses cookies to ensure you get the best experience here.",
+      "href": "https://trialsnet.eu/tos/"
+    }
+  });
